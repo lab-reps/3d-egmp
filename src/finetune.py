766426fc288650ed48a9456f4d96f0df4feb0dbf
Sherlocked_hzoi <Sherlocked_hzoi@163.com>
2023-11-22 18:12:32 +0800

    geom.py, pretrain.py, finetune.py completed

--- src/finetune.py
+++ src/finetune.py
@@ -1,3 +1,170 @@
+import argparse
 import model
+import yaml
+from easydict import EasyDict
+from torch import nn, optim
+from torch.nn import functional as F
+from data import qm9, utils
 
-model=model.EGNN_qm9(in_node_nf)
+all_tasks=['alpha', 'gap', 'homo', 'lumo', 'mu', 'Cv', 'G', 'H', 'r2', 'U', 'U0', 'zpve']
+parser=argparse.ArgumentParser(description='QM9 Example')
+parser.add_argument('--config-path', type=str, default='.', metavar='N', help='Path of config yaml.')
+parser.add_argument('--property', type=str, default='', metavar='N', help='Property to predict.')
+parser.add_argument('--model_name', type=str, default='', metavar='N', help='Model name.')
+parser.add_argument('--restore_path', type=str, default='', metavar='N', help='Restore path.')
+args=parser.parse_args()
+
+device=torch.device('cuda')
+dtype=torch.float32
+with open(args.config_path, 'r') as f:
+    config=yaml.safe_load(f)
+config=EasyDict(config)
+
+if args.property!='': config.train.property=args.property
+if args.model_name!='': config.model.name=args.model_name
+if args.restore_path!='': config.train.restore_path=args.restore_path
+
+os.makedirs(config.train.save_path+'/'+config.model.name+'/'+config.train.property, exist_ok=True)
+
+seed=config.train.seed
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+print('fix seed:', seed)
+
+def load_model(model, model_path):
+    state=torch.load(model_path, map_location=device)
+    new_dict=OrderedDict()
+    for k,v in state['model'].items():
+        if k.startswith('module.model.'):
+            new_dict[k[13:]]=v
+        if k.startswith('model.'):
+            new_dict[k[6:]]=v
+    model.load_state_dict(new_dict, strict=False)
+    return new_dict
+
+dataloaders, charge_scale=qm9.retrieve_dataloaders(config.data.base_path, config.train.batch_size, config.train.num_workers)
+mean, mad=utils.compute_mean_mad(dataloaders, config.train.property)
+
+model=model.EGNN_qm9(in_node_nf=config.model.max_atom_type*(config.model.charge_power+1),
+                    in_edge_nf=0 if config.model.no_edge_type else 4, hidden_nf=config.model.hidden_dim,
+                    attention=config.model.attention, use_layer_norm=config.model.layernorm).to(device)
+
+if config.train.restore_path:
+    encoder_param=load_model(model, config.train.restore_path)
+    print('load model from', config.train.restore_path)
+
+optimizer=optim.Adam([param for name, param in model.named_parameters()], lr=config.train.lr, weight_decay=float(config.train.weight_decay))
+lr_scheduler=optim.lr_scheduler.CosineAnnealingLR(optimizer, config.train.epochs, eta_min=float(config.train.min_lr))
+loss_l1=nn.L1Loss()
+
+def process_input(atom_type, max_atom_type=100, charge_power=2):
+    one_hot=F.one_hot(atom_type, max_atom_type)
+    charge_tensor=(atom_type.unsqueeze(-1)/max_atom_type).pow(torch.arange(charge_power+1, dtype=torch.float32).to(atom_type)).view(atom_type.shape+(1, charge_power+1))
+    atom_scalars=(one_hot.unsqueeze(-1)*charge_tensor).view(atom_type.shape+(-1,))
+    return atom_scalars
+
+def binarize(x):
+    return torch.where(x>0.5, torch.ones_like(x), torch.zeros_like(x))
+
+def get_higher_order_A(adj, order):
+    A=[torch.eye(adj.size(0), device=adj.device), binarize(adj+torch.eye(adj.size(0), device=adj.device))]
+    for i in range(1, order+1):
+        A_mats.append(binarize(adj_mats[i-1])@adj_mats[1])
+    order=torch
+
+def gen_fc(pos, mask, with_hop=True):
+    batch, nodes=mask.shape
+    if with_hop: 
+        batch_A=torch.norm(pos.unsqueeze(1)-pos.unsqueeze(2), p=2, dim=-1)
+    batch_mask=(mask[:,:,None]*mask[:, None, :]).bool()&(~with_hop|(batch_A<=1.6))&(~torch.eye(nodes).to(mask).bool())
+    batch_mask=torch.block_diag(*batch_mask)
+    if with_hop:
+        A_order=get_higher_order_A(batch_mask, 3)
+        type_highorder=torch.where(A_order>1, A_order, torch.zeros_like(A_order))
+        fc_mask=batch_mask_fc.bool() & (~torch.eye(nodes).to(mask).bool())
+        fc_mask=torch.block_diag(*fc_mask)
+        type_new=batch_mask+type_highorder+fc_mask
+    edge_index, edge_type=dense_to_sparse(batch_mask)
+    return edge_index, edge_type - with_hop
+
+def train(epoch, dataloader, config, partition='train'):
+    res={'loss': 0, 'counter': 0, 'loss_arr': []}
+    for i,data in enumerate(dataloader):
+        if partition=='train':
+            model.train()
+            optimizer.zero_grad()
+        else:
+            model.eval()
+
+        batch_size, n_nodes, _=data['positions'].size()
+        atom_positions=data['positions'].view(batch_size, n_nodes, -1).to(device, dtype)
+        
+        atom_mask=data['atom_mask'].view(batch_size, n_nodes).to(device)
+        edge_mask=data['edge_mask'].to(device)
+
+        charges=data['charges'].to(device, torch.long).view(charges, max_atom_type=config.model.max_atom_type, charge_power=config.model.charge_power)
+        nodes=process_input(charges, max_atom_type=config.model.max_atom_type, charge_power=config.model.charge_power).view(batch_size*n_nodes,-1)
+        
+        edges, edge_types=gen_fc(atom_positions, atom_mask,with_hop=not config.model.no_edge_types)
+        edge_attr=None if config.model.no_edge_types else F.one_hot(edge_types, 4)
+
+        label=data[config.train.property].to(device, dtype)
+        atom_positions=atom_positions.view(batch_size*n_nodes, -1)
+        atom_mask=atom_mask.view(batch_size*n_nodes, -1)
+
+        pred=model(nodes, atom_positions, edges, edge_attr, atom_mask, n_nodes, config.train.property)
+
+        if partition=='train':
+            loss=loss_l1(pred, (label-mean)/mad)
+            loss.backward()
+            optimizer.step()
+        else: 
+            loss=loss_l1(mad*pred+mean, label)
+        
+        res['loss']+=loss.item()*batch_size
+        res['counter']+=batch_size
+        res['loss_arr'].append(loss.item())
+
+        prefix=''
+        if partition!='train':
+            prefix='>> %s \t'%partition
+
+        if i%config.train.log_interval==0:
+            print(prefix, "epoch: %4d | Iter: %4d | loss: %.4f | lr: %.5f"%(epoch, i, sum(res['loss_arr'][-10:])/len(res['loss_arr'][-10:]),
+                                                                            optimizer.state_dict()['param_groups'][0]['lr']))
+
+    return res['loss']/res['counter']
+
+if __name__=='__main__':
+    res={'epochs': [], 'losses': [], 'best_val': 1e10, 'best_test': 1e10, 'best_epoch': 0}
+    all_train_loss, all_val_loss, all_test_loss=[], [], []
+    path=config.train.save_path+'/'+config.model.name+'/'+config.train.property
+    for epoch in range(0, config.train.epochs):
+        train_loss=train(epoch, dataloaders['train'], config, partition='train')
+        all_train_loss.append(train_loss)
+        lr_scheduler.step()
+        if epoch%config.test.test_interval==0:
+            val_loss=train(epoch, dataloaders['valid'], config, patition='valid')
+            test_loss=train(epoch, dataloaders['test'], config, partition='test')
+            res['epochs'].append(epoch)
+            res['losses'].append(test_loss)
+            if val_loss<res['best_val']:
+                res['best_val'], res['best_test'], res['best_epoch']=val_loss, test_loss, epoch
+                state={
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict(),
+                    'scheduler': lr_scheduler.state_dict(),
+                    'epoch': epoch
+                }
+                torch.save(state, path+'/checkpoint_best.pth')
+
+            print("val loss: %.4f \t test loss: %.4f \t epoch %d"%(val_loss, test_loss, epoch))
+            print("best: val loss: %.4f \t test loss: %.4f \t epoch %d"%(res['best_val'], res['best_test'], res['best_epoch']))
+            all_val_loss.append(val_loss)
+            all_test_loss.append(test_lodd)
+
+        with open(path+'/loss.pkl', 'wb') as f:
+            pkl.dump((all_train_loss, all_val_loss, all_test_loss), f)
+
--- src/pretrain.py
+++ src/pretrain.py
@@ -1,13 +1,144 @@
+from time import time
 import torch.distributed as dist
 
-def train(rank, config, world_size):
+def train(rank, config, world_size=1):
 
     print('Rank:',rank)
     verbose=(rank==0)
 
     train_start=time()
+    transform=Compose([
+        Cutoff(cutoff_length=config.model.cutoff) if config.model.no_edge_types else EdgeHop(max_hop=config.model.order),
+        AtomOnehot(max_atom_type=config.model.max_atom_type, charge_power=config.model.charfe_power)
+    ])
 
-gpu_cnt=torch.cuda.device_count()
-if gpu_cnt>1:
-    dist.init_process_group('nccl', rank=args.local_rank, world_size=gpu_cnt)
-train(args.local_rank, args.config, gpu_cnt)
+    np.random.seed(config.train.seed)
+    random.seed(config.train.seed)
+    torch.manual_seed(config.train.seed)
+
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(config.train.seed)
+        torch.cuda.manual_seed_all(config.train.seed)
+    torch.backends.cudnn.benchmark=True
+
+    data_dir=config.data.block_dir
+    with open(os.path.join(data_dir, 'summary.json'), 'r') as f:
+        summary=json.load(f)
+    train_block_num=summary['train block num']
+    train_block_size=summary['train block size']
+    val_block_size=summary['val block size']
+
+    val_block=BatchDatapoint(os.path.join(data_dir, 'val_clock.pkl'), val_block_size)
+    val_block.load_datapoints()
+    val_dataset=GEOMDataset([val_block], val_block_size, transforms=transform)
+
+    train_blocks=[BatchDatapoint(os.path.join(data_dir, 'train_clock_%d.pkl'%i), train_block_size) for i in range(train_block_num)]
+    for d in train_blocks: d.load_datapoints()
+    train_dataset=GEOMDataset(train_blocks, train_block_size, transforms=transform)
+
+    edge_types=(0 if config.model.no_edge_types else config.model.order+1)
+    rep=EGNN(in_node_nf=config.model.max_atom_type*(config.model.charge_power+1),
+                    in_edge_nf=edge_types, hidden_nf=config.model.hidden_dim, n_layers=config.model.n_layers, 
+                    attention=config.model.attention, use_layer_norm=config.model.layernorm)
+    model=EquivariantDenoisePred(config, rep)
+    num_epochs=config.train.epochs
+    dataloader=DataLoader(train_dataset, batch_size=config.train.batch_size, shuffle=True, num_workers=config.train.num_workers, pin_memory=False)
+    valloader=DataLoader(val_dataset, batch_size=config.train.batch_size, shuffle=False, num_workers=config.train.num_workers)
+
+    model=model.to(rank)
+    optimizer=utils.get_optimizer(config.train.optimizer, model)
+    scheduler=utils.get_scheduler(config.train.scheduler, optimizer)
+    train_losses, val_losses=[], []
+    ckpt_list=[]
+    max_ckpt_maintain=10
+    best_loss=100.0
+    start_epoch=0
+
+    print(f'Rank {rank} start training...')
+
+    for epoch in range(num_epochs):
+        model.train()
+        epoch_start=time()
+        batch_losses, batch_cnt=[], 0
+        for batch in dataloader:
+            batch_cnt+=1
+            batch=batch.to(rank)
+            loss_denoise, loss_pred_noise=model(batch)
+            loss=config.train.loss_pred_noise*loss_pred_noise+config.train.loss_denoise*loss_denoise
+            if not loss.requires_grad:
+                raise RuntimeError('loss doesn\'t require grad')
+            optimizer.zero_grad()
+            loss.backward()
+            norm=nn.utils.clip_grad_norm_(model.parameters(), config.train.clip)
+            if not norm.isnan:
+                optimizer.step()
+            batch_losses.append(loss.item())
+
+            if verbose and (batch_cnt%config.train.log_interval==0 or (epoch==0 and batch_cnt<10<=10)):
+                print('Epoch: %d | step: %d | loss: %.5f(%.3f/%.3f) | GradNorm: %.5f | lr: %.5f' %
+                            (epoch+start_epoch, batch_cnt, batch_losses[-1], loss_pred_noise.item(), loss.denoise.item(), norm.item(), optimizer.param_groups[0]['lr']))
+
+        average_loss=sum(batch_losses)/len(batch_losses)
+        train_losses.append(average_loss)
+
+        if verbose:
+            print('Epoch: %d | train loss: %.5f | time : %.5f'%(epoch+start_epoch, average_loss, time.time()-start_time))
+        
+        scheduler.step()
+        model.eval()
+        eval_start=time()
+        eval_losses=[]
+        for batch in valloader:
+            batch=batch.to(rank)
+            loss_denoise, loss_pred_noise=model(batch)
+            loss=config.train.loss_pred_noise*loss_pred_noise+config.train.loss_denoise*loss_denoise
+            eval_losses.append(loss.item())
+        eval_loss=sum(eval_losses)/len(eval_losses)
+        print('Evaluate val loss: %.5f | time: %.5f' % (average_loss, time()-eval_start))
+        val_losses.append(average_loss)
+        if val_losses[-1]<best_loss:
+            best_loss=val_losses[-1]
+            if config.train.save:
+                state={
+                    'model': model.state_dict(),
+                    'config': config,
+                    'cur_epoch': epoch+start_epoch,
+                    'best_loss': best_loss,
+                }
+                epoch=str(epoch) if epoch is not None else ''
+                checkpoint=os.path.join(config.train.save_path, 'checkpoint%s'%epoch)
+                if len(skpt_list)>=max_ckpt_maintain:
+                    try: os.remove(ckpt_list[0])
+                    except: print('Remove checkpoint failed for', ckpt_list[0])
+                    ckpt_list=ckpt_list[1:]
+                ckpt_list.append(checkpoint)
+                torch.save(state, checkpoint)
+
+    best_loss=best_loss
+    start_epoch=epoch+num_epochs
+    print('optimization finished.')
+    print('Total time elapsed: %.5fs' %(time()-train_start))
+
+if __name__=='__main__':
+    torch.set_printoptions(profile="full")
+    parser=argparse.ArgumentParser(description='mgp')
+    parser.add_argument('--config_path', type=str, help='path of dataset', required=True)
+    parser.add_argument('--seed', type=int, default=2021, help='overwrite config seed')
+    parser.add_argument('--steps', type=int, default=0, help='overwrite config steps')
+    parser.add_argument('--local_rank', type=int, default=0)
+    args=parser.parse_args()
+
+    with open(args.config_path,'r') as f:
+        config=yaml.safe_load(f)
+    config.EasyDict(config)
+    if args.seed!=2021: config.train.seed=args.seed
+    if args.steps!=0: config.train.steps=args.steps
+    if config.train.save and config.train.save_path is not None:
+        config.train.save_path=os.path.join(config.train.save_path, config.model.name)
+        if not os.path.exists(config.train.save_path):
+            os.makedirs(config.train.save_path, exist_ok=True)
+        
+    print(config)
+    train(args.local_rank, config, world_size)
+
+
